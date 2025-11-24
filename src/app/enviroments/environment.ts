82a3eac7945237f5908a759f43d80fe51d418b64
igrisdev <johanalvarezz237@gmail.com>
2025-11-24 18:34:13 -0500

    feat: Add environment configuration file with API base URL.

--- src/app/enviroments/environment.ts
+++ src/app/enviroments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  // apiBase: 'http://localhost:5217/api'
-  apiBase: 'https://unireportes-api.apolobyte.top'
+  apiBase: 'http://localhost:5217/api'
+  // apiBase: 'https://unireportes-api.apolobyte.top'
 };
